bc7322e76e74511c5788b148e2654e118de59219
sakshamagrawall <sakshamagrawal18@gmail.com>
2023-01-12 10:46:22 +0530

    class component to function component

--- src/components/NewItem.js
+++ src/components/NewItem.js
@@ -1,27 +1,36 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class NewItem extends Component {
-  
-  render() {
-    let { title, description, imageUrl, newsUrl,author,date,source} = this.props;
-    return (
-      <>
+const NewItem = (props) => {
+  let { title, description, imageUrl, newsUrl, author, date, source } = props;
+  return (
+    <>
       <div className="card">
-        <div style={{display:"flex",justifyContent:"flex-end",position:"absolute",right:'0'}}>
-      <span className="badge rounded-pill bg-danger">{source}</span>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            position: "absolute",
+            right: "0",
+          }}
+        >
+          <span className="badge rounded-pill bg-danger">{source}</span>
+        </div>
+        <img src={imageUrl} className="card-img-top" alt="News img" />
+        <div className="card-body">
+          <h5 className="card-title">{title}...</h5>
+          <p className="card-text">{description}...</p>
+          <a href={newsUrl} target="blank" className="btn btn-sm btn-primary">
+            Read more
+          </a>
+          <p className="card-text">
+            <small className="text-muted">
+              by {author} on {new Date(date).toGMTString()}
+            </small>
+          </p>
+        </div>
       </div>
-  <img src={imageUrl} className="card-img-top" alt="News img"/>
-  <div className="card-body">
-    <h5 className="card-title">{title}... 
-</h5>
-    <p className="card-text">{description}...</p>
-    <a href={newsUrl} target="blank" className="btn btn-sm btn-primary">Read more</a>
-    <p className="card-text"><small className="text-muted">by {author} on {new Date(date).toGMTString()}</small></p>
-  </div>
-</div>
-      </>
-    )
-  }
-}
+    </>
+  );
+};
 
 export default NewItem;
--- src/components/News.js
+++ src/components/News.js
@@ -1,113 +1,109 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import NewItem from "./NewItem";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-export class News extends Component {
-  static defaultProps = {
-    country: "us",
-    pageSize: 8,
-    category: "general",
-  };
-  static propTypes = {
-    country: PropTypes.string,
-    category: PropTypes.string,
-    pageSize: PropTypes.number,
-  };
-  capitalString=(string)=>{
-    return string.charAt(0).toUpperCase()+string.slice(1);
-  }
+const News = (props) => {
+  const [articles, setArticals] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      articles: [],
-      loading: true,
-      page:1,
-      totalResults:0
-    };
-    document.title=`${this.capitalString(this.props.category)}-NewsMaza`
-  }
-  async updateNews() {
-    this.props.setProgress(10);
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true });
+  const capitalString = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+  const updateNews = async () => {
+    props.setProgress(10);
+    let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+    setLoading(true);
     let data = await fetch(url);
-    this.props.setProgress(30);
+    props.setProgress(30);
     let parsedData = await data.json();
-    this.props.setProgress(70);
-    this.setState({
-      articles: parsedData.articles,
-      totalResults: parsedData.totalResults,
-      loading: false
-    });
-    this.props.setProgress(100);
-  }
-  async componentDidMount() {
-    this.updateNews();
-  }
-  // handlePreviousClick = async () => {
-  //   this.setState({ page: this.state.page - 1 });
-  //   this.updateNews();
-  // };
-  // handleNextClick = async () => {
-  //   if (
-  //     !(
-  //       this.state.page + 1 >
-  //       Math.ceil(this.state.totalResults / this.props.pageSize)
-  //     )
-  //   ) {
-  //     this.setState({ page: this.state.page + 1 });
-  //     this.updateNews();
-  //   }
-  // };
-  fetchMoreData=async ()=>{
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+    props.setProgress(70);
+    setArticals(parsedData.articles);
+    setTotalResults(parsedData.totalResults);
+    setLoading(false);
+    props.setProgress(100);
+  };
 
+  useEffect(() => {
+    document.title = `${capitalString(props.category)}-NewsMaza`;
+    updateNews();
+    //eslint-disable-next-line
+  }, []);
+
+  const fetchMoreData = async () => {
+    setPage(page + 1);
+    let url = `https://newsapi.org/v2/top-headlines?country=${
+      props.country
+    }&category=${props.category}&apiKey=${props.apiKey}&page=${
+      page + 1
+    }&pageSize=${props.pageSize}`;
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.setState({page:this.state.page+1,
-      articles: this.state.articles.concat(parsedData.articles),
-      totalResults: parsedData.totalResults
-    });
-    
-  }
-  render() {
-    return (
-      <>
-          <h1 className="text-center" style={{margin:"30px 0px",marginTop:"90Px"}}>{`NewMaza-Top Headlines from ${this.capitalString(this.props.category)}`}
-          </h1>
-        
-          {this.state.loading && <Spinner />}
+    setTotalResults(parsedData.totalResults);
+    setArticals(articles.concat(parsedData.articles));
+  };
+  return (
+    <>
+      <h1
+        className="text-center"
+        style={{ margin: "30px 0px", marginTop: "90Px" }}
+      >
+        {`NewMaza-Top Headlines from ${capitalString(props.category)}`}
+      </h1>
 
-          <InfiniteScroll 
-          dataLength={this.state.articles.length}
-          next={this.fetchMoreData}
-          hasMore={this.state.articles.length !== this.state.totalResults}
-          loader={<Spinner/>}
-          >
-          <br/>
-          <div className="container">
+      {loading && <Spinner />}
+
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={fetchMoreData}
+        hasMore={articles.length !== totalResults}
+        loader={<Spinner />}
+      >
+        <br />
+        <div className="container">
           <div className="row">
-            {this.state.articles.map((element) => {
+            {articles.map((element) => {
               return (
                 <div className="col-md-4" key={element.url}>
-                  <NewItem source={element.source.name} author={element.author ? element.author : "unknown"} date={element.publishedAt ? element.publishedAt : ""} title={element.title ? element.title.slice(0, 20) : ""} description={element.description ? element.description.slice(0, 88): ""}
-                      imageUrl={element.urlToImage ? element.urlToImage:"https://gumlet.assettype.com/newslaundry%2F2022-09%2F68b6146a-f40a-45a0-b2b2-6e4292144fd7%2Ferror_newsnine.jpg?auto=format%2Ccompress&fit=max&dpr=1.0&q=70&w=1200"} newsUrl={element.url}/>
-                  </div>
-                )
-              })
-            }
-            </div>
-            </div>
-        </InfiniteScroll>
-        
+                  <NewItem
+                    source={element.source.name}
+                    author={element.author ? element.author : "unknown"}
+                    date={element.publishedAt ? element.publishedAt : ""}
+                    title={element.title ? element.title.slice(0, 20) : ""}
+                    description={
+                      element.description
+                        ? element.description.slice(0, 88)
+                        : ""
+                    }
+                    imageUrl={
+                      element.urlToImage
+                        ? element.urlToImage
+                        : "https://gumlet.assettype.com/newslaundry%2F2022-09%2F68b6146a-f40a-45a0-b2b2-6e4292144fd7%2Ferror_newsnine.jpg?auto=format%2Ccompress&fit=max&dpr=1.0&q=70&w=1200"
+                    }
+                    newsUrl={element.url}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </InfiniteScroll>
+    </>
+  );
+};
 
-      
-      </>
-    );
-  }
-}
+News.defaultProps = {
+  country: "us",
+  pageSize: 8,
+  category: "general",
+};
+News.propTypes = {
+  country: PropTypes.string,
+  category: PropTypes.string,
+  pageSize: PropTypes.number,
+};
 
 export default News;
